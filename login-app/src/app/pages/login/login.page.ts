99a3bcb91efff00d7be57139fa81783850bfc12e
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2023-02-28 14:46:40 +0300

    users can login & register

--- login-app/src/app/pages/login/login.page.ts
+++ login-app/src/app/pages/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { FireserviceService } from 'src/app/fireservice.service';
 import { LoginPageForm } from './login.page.form';
 
 @Component({
@@ -9,15 +10,32 @@ import { LoginPageForm } from './login.page.form';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  public email:any;
+  public password:any;
+
   form!: FormGroup;
-  constructor(private router: Router, private formBuilder: FormBuilder) { }
+  constructor(private router: Router, private formBuilder: FormBuilder, public fireService: FireserviceService) { }
 
   ngOnInit() {
     this.form = new LoginPageForm(this.formBuilder).createForm();
   }
 
   login(){
-    this.router.navigate(['home']);
+    this.fireService.loginWithEmail({email:this.email,password:this.password}).then(res=>{
+      console.log(res);
+      if(res.user?.uid){
+        this.fireService.getDetails({uid:res.user.uid}).subscribe(res=>{
+          console.log(res);
+          //alert('Welcome '+ res['name']);
+          this.router.navigate(['home']);
+        },err=>{
+          console.log(err);
+        });
+      }
+    },err=>{
+      alert(err.message)
+      console.log(err);
+    })
   }
 
   register(){
--- login-app/src/app/pages/register/register.page.ts
+++ login-app/src/app/pages/register/register.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FireserviceService } from 'src/app/fireservice.service';
 
 @Component({
   selector: 'app-register',
@@ -7,14 +8,36 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-
-  constructor(private router: Router) { }
+  public email:any;
+  public password:any;
+  public name:any;
+  constructor(private router: Router, public fireService:FireserviceService) { }
 
   ngOnInit() {
   }
 
   register(){
-    this.router.navigate(['home']);
-  }
+    this.fireService.signup({email:this.email, password:this.password}).then(res=>{
+      if(res.user?.uid){
+        let data = {
+          email:this.email,
+          password:this.password,
+          name:this.name,
+          uid:res.user.uid
+        }
+        this.fireService.saveDetails(data).then(res=>{
+         alert('Account Created!');
+         this.router.navigate(['home']);
+        },err=>{
+          console.log(err);
+        })
+      }
+    },err=>{
+      alert(err.message);
 
+      console.log(err);
+    })
+  }
 }
+
+
